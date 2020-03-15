345d6cfafbc1ea0f95e11b0abb617839c6486d4d
damonlau27 <lygm1062@gmail.com>
2020-03-16 02:26:14 +0800

    husky's birthday

---
+++ precache-manifest.7dfc5055f0e49c145310a900d5ee7714.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ee80387b3194d8efef71bdf9d18b4b",
+    "url": "/husky/index.html"
+  },
+  {
+    "revision": "3fb2391d590a2ca12700",
+    "url": "/husky/static/css/2.2d82fa2a.chunk.css"
+  },
+  {
+    "revision": "ca557a2afdeea5779c63",
+    "url": "/husky/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "3fb2391d590a2ca12700",
+    "url": "/husky/static/js/2.2add30d0.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/husky/static/js/2.2add30d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca557a2afdeea5779c63",
+    "url": "/husky/static/js/main.18ac806d.chunk.js"
+  },
+  {
+    "revision": "24e9c0ba452edd35cebd",
+    "url": "/husky/static/js/runtime-main.5c77d2e7.js"
+  }
+]);
